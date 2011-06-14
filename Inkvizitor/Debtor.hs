732848f3528234c56e3751b1127e4732b440c0df
Honza Špaček <patek.mail@gmail.com>
2011-06-14 21:13:54 +0200

    Save and read execution date from JSON.
    Add support for saving and reading "execution time" field of debtor.

--- Inkvizitor/Debtor.hs
+++ Inkvizitor/Debtor.hs
@@ -6,6 +6,8 @@ module Inkvizitor.Debtor
   )
 where
 
+import Control.Applicative
+import Data.Fixed
 import Data.Time
 import qualified Data.Map as Map
 import Text.JSON
@@ -18,7 +20,7 @@ data Debtor = Debtor
   , getExecutionNum :: String
   , getAddresses :: [String]
   , getAmount :: Int
-  , getExecutionTime :: Maybe UTCTime
+  , getExecutionTime :: Maybe LocalTime
   , getComment :: String
   } deriving (Eq, Show)
 
@@ -35,6 +37,29 @@ getDebtorsFromJSON txt =
 getJSONFromDebtors :: DebtorMap -> String
 getJSONFromDebtors = (++"\n") . PP.render . pp_value . showJSON
 
+instance JSON Day where
+  readJSON val = do
+    (year,month,day) <- readJSON val
+    return $ fromGregorian year month day
+  showJSON =
+    showJSON . toGregorian
+
+instance JSON TimeOfDay where
+  readJSON val = do
+    (hour, min, sec) <- readJSON val
+    case makeTimeOfDayValid hour min (fromIntegral (sec :: Int)) of
+      Just tod -> return tod
+      Nothing -> fail "invalid time of day"
+  showJSON tod =
+    showJSON (todHour tod, todMin tod, (truncate $ todSec tod) :: Int)
+
+instance JSON LocalTime where
+  readJSON val = do
+    (day, tod) <- readJSON val
+    LocalTime <$> readJSON day <*> readJSON tod
+  showJSON loc =
+    showJSON (showJSON $ localDay loc, showJSON $ localTimeOfDay loc)
+
 instance JSON Debtor where
 
   readJSON (JSObject jsobj) = do
@@ -43,7 +68,7 @@ instance JSON Debtor where
     exNum <- field "ex_num"
     addresses <- field "addresses"
     amount <- field "amount"
-    exTime <- return Nothing -- TODO
+    exTime <- field "ex_time"
     comment <- field "comment"
 
     return Debtor
@@ -64,7 +89,7 @@ instance JSON Debtor where
               Nothing -> Error $ "expected field " ++ show key
 
   showJSON debtor =
-    makeObj $ map (\(key,val) -> (key, val debtor)) fields
+    makeObj $ map (\(key,fun) -> (key, fun $ debtor)) fields
     where fields :: [(String, Debtor -> JSValue)]
           fields =
             [ ("name",      showJSON . getName)
@@ -72,6 +97,6 @@ instance JSON Debtor where
             , ("ex_num",    showJSON . getExecutionNum)
             , ("addresses", showJSON . map toJSString . getAddresses)
             , ("amount",    showJSON . getAmount)
-            , ("ex_time",   showJSON . const (Nothing :: Maybe JSValue)) -- TODO
+            , ("ex_time",   showJSON . getExecutionTime) 
             , ("comment",   showJSON . getComment)
             ]
